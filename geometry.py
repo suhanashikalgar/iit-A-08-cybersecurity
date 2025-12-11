22c95da31f8dcc5bac717fc39317a59f0c0b435f
Suhana Shikalgar <suhanashikalgar2@gmail.com>
2025-12-11 09:13:32 +0530

    react area perimeter

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def calc_rect_area(length, width):
+    return length * width
+
+def calc_rect_perimeter(length, width):
+    return 2 * (length + width)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import arithmatic
+import geometry as geo
+from geometry import calc_rect_perimeter
 
 print("hello world")
 print("Addition:",arithmatic.add(5,3))
+print("Rectangle Perimeter:",calc_rect_perimeter(4,6))  
